8c6ec39e4fab5f91306dc5d71c3a71576f4cc519
Hanna Ostlund <143076946+hannaostlund@users.noreply.github.com>
2024-01-06 22:24:28 +0100

    added js footer and some more styling
    added some new fonts,

---
+++ JavaScript/footer.js
@@ -0,0 +1,26 @@
+//setting up a js reusable footer the same way as the header js i did before
+document.addEventListener('DOMContentLoaded', function() {
+    const footerTemplate = document.createElement('footer');
+    footerTemplate.classList.add('site-footer');
+    footerTemplate.innerHTML = `
+        <div class="name-footer">
+            <h1 class="footer-h1">Get in Touch</h1>
+            <p class="footer-text">Hit me up on social media! Always looking for new exciting opportunities</p>
+        </div>
+        <div class="social-icons">
+            <a href="/">
+                <img src="/Images/linkedin.svg" alt="Linkedin icon" />
+            </a>     
+            <a href="/">
+                <img src="/images/githubicon.svg" alt="Github icon" />
+            </a>     
+        </div>
+    `;
+
+    const footerPlaceholder = document.getElementById('footer-placeholder'); // placeholder in HTML
+    if (footerPlaceholder) {
+        footerPlaceholder.appendChild(footerTemplate);
+    } else {
+        console.error('Footer placeholder element not found.');
+    }
+});
