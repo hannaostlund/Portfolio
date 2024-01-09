e871b8a2c8eae6bf9d49b7e5b87aaf4e1117223a
Hanna Ostlund <143076946+hannaostlund@users.noreply.github.com>
2024-01-09 22:36:08 +0100

    added some links to the footer
    And added a education under cv

--- JavaScript/footer.js
+++ JavaScript/footer.js
@@ -5,15 +5,15 @@ document.addEventListener('DOMContentLoaded', function() {
     footerTemplate.innerHTML = `
         <div class="name-footer">
             <h1 class="footer-h1">Get in Touch</h1>
-            <p class="footer-text">Hit me up on social media! Always looking for new exciting opportunities</p>
+            <p class="footer-text">Let's connect! Always looking for new exciting opportunities</p>
         </div>
         <div class="social-icons">
-            <a href="/">
-                <img src="/Images/linkedin.svg" alt="Linkedin icon" />
-            </a>     
-            <a href="/">
-                <img src="/images/githubicon.svg" alt="Github icon" />
-            </a>     
+        <a href="https://www.linkedin.com/in/hanna-%C3%B6stlund-197214ba">
+            <img src="/Images/linkedin.svg" alt="Linkedin icon" />
+        </a>
+        <a href="https://github.com/hannaostlund">
+            <img src="/images/githubicon.svg" alt="Github icon" />
+        </a>   
         </div>
     `;
 
